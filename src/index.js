01109b944a747448a07f65cb11c871ca1ac30766
Gustavo Moreno <gusadolfo123@hotmail.com>
2021-11-08 17:03:12 -0500

    fix(index): bugfix example

--- src/index.js
+++ src/index.js
@@ -159,6 +159,10 @@ export function initialize({ basePath, urlSignIn, userName, accessKey }) {
   ApiClient.initialize({ basePath, urlSignIn, userName, accessKey });
 }
 
+export function prueba(){
+  return "prueba";
+}
+
 /**
 * Siigo_Api_v1.<br>
 * The <code>index</code> module provides access to constructors for all the classes which comprise the public API.
